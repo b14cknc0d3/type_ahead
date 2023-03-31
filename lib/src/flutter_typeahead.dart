27dcebd1ac8674ae67d83441c41fbec620dc8f89
YeLinAung <40236753+b14cknc0d3@users.noreply.github.com>
2023-03-31 11:15:49 +0630

    testing

--- lib/src/flutter_typeahead.dart
+++ lib/src/flutter_typeahead.dart
@@ -1463,7 +1463,7 @@ class _SuggestionsListState<T> extends State<_SuggestionsList<T>>
     }
 
     return SizedBox(
-        height: MediaQuery.of(context).size.height * 0.5, child: child);
+        height: MediaQuery.of(context).size.height * 0.5 - 32, child: child);
   }
 }
 
